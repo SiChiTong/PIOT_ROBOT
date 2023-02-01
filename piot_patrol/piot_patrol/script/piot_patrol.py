23a4d5e82c44a0a31b504b54dcf6fc86550c077a
PIOT-KGH <122580386+PIOT-KGH@users.noreply.github.com>
2023-02-01 14:13:42 +0900

    Add files via upload

---
+++ piot_patrol/piot_patrol/script/piot_patrol.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+
+import time
+from copy import deepcopy
+
+from geometry_msgs.msg import PoseStamped
+from rclpy.duration import Duration
+import rclpy
+
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+
+def main():
+    rclpy.init()
+
+    navigator = BasicNavigator()
+
+    security_route = [
+        [1.792, 2.144],
+        [1.792, -5.44],
+        [1.792, -9.427],
+        [-3.665, -9.427],
+        [-3.665, -4.303],
+        [-3.665, 2.330],
+        [-3.665, 9.283]]
+
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    initial_pose.header.stamp = navigator.get_clock().now().to_msg()
+    initial_pose.pose.position.x = 3.45
+    initial_pose.pose.position.y = 2.15
+    initial_pose.pose.orientation.z = 1.0
+    initial_pose.pose.orientation.w = 0.0
+    navigator.setInitialPose(initial_pose)
+
+    navigator.waitUntilNav2Active()
+
+    while rclpy.ok():
+        route_poses = []
+        pose = PoseStamped()
+        pose.header.frame_id = 'map'
+        pose.header.stamp = navigator.get_clock().now().to_msg()
+        pose.pose.orientation.w = 1.0
+        for pt in security_route:
+            pose.pose.position.x = pt[0]
+            pose.pose.position.y = pt[1]
+            route_poses.append(deepcopy(pose))
+        navigator.goThroughPoses(route_poses)
+
+        # Do something during our route
+		#
+		
+        security_route.reverse()
+
+        result = navigator.getResult()
+        if result == TaskResult.SUCCEEDED:
+            print('Route complete! Restarting...')
+        elif result == TaskResult.CANCELED:
+            print('Security route was canceled, exiting.')
+            exit(1)
+        elif result == TaskResult.FAILED:
+            print('Security route failed! Restarting from other side...')
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
